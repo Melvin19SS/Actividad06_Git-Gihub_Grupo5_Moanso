5dde2c935242f2969b5d56045b92293c37cacd48
Leo <alejandrolescano442@gmail.com>
2024-09-28 23:59:15 -0500

    se hizo todos los cambios

--- Actividad_N06_moanso_Grupo5/Actividad_N06_moanso_Grupo5/Form1.cs
+++ Actividad_N06_moanso_Grupo5/Actividad_N06_moanso_Grupo5/Form1.cs
@@ -16,5 +16,10 @@ namespace Actividad_N06_moanso_Grupo5
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          // hello
+        }
     }
 }
